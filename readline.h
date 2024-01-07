35d598c89f4035c923d6b275635167804c044e0f
rebeccasun31 <rebeccasun31@gmail.com>
2024-01-07 16:18:11 +0800

    readline

---
+++ readline.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include    <stdio.h>
+#include    <unistd.h>
+#include    <string>
+#include    <cstring>
+
+using namespace std;
+
+string receivedData;
+ssize_t readline(int fd, char* buffer, size_t bufferSize) {
+    while (true) {
+        size_t newlinePos = receivedData.find('\n');
+        if (newlinePos != string::npos) {
+            string line = receivedData.substr(0, newlinePos + 1);
+
+            receivedData.erase(0, newlinePos + 1);
+
+            if (line.find('\n') != string::npos) {
+                strcpy(buffer, line.c_str());
+                break;
+            }
+        }
+        size_t bytesRead = read(fd, buffer, bufferSize);
+        if (bytesRead <= 0) {
+            return bytesRead;
+        }
+        buffer[bytesRead] = 0;
+
+        receivedData.append(buffer, bytesRead);
+    }
+    return strlen(buffer);
+}
