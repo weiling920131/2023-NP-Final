bec56e7105908d0d8d5c4dbeea60963d155c45f5
weiling920131 <weiling920131@gmail.com>
2024-01-08 22:14:36 +0800

    server

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-g++ -o server server.cpp slither.cc -lnsl -lpthread
-g++ -o client client.cpp slither.cc
+g++ -o server ./source/server.cpp ./source/slither.cc -lnsl -lpthread
+g++ -o client ./source/client.cpp ./source/slither.cc
